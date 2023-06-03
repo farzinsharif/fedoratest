828658aee87d39903405f0b4867709446fc6611a
Farzinsharif <farzinsharif.j@gmail.com>
2023-06-03 06:15:15 -0400

    sample

---
+++ t.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -d /etc/pacman.d ]
+then
+    sudo pacman -syu
+fi
+if [ -d /etc/apt ]
+then
+    sudo apt update
+    sudo apt dist-upgrade
+fi
+if [ -d /etc/dnf ]
+then
+    echo "eyvollah this is fedora dist"
+fi
